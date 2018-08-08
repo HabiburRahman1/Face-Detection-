5d4597088591d9f107ea251699de06b4fa3e8004
Md Habibur Rahman <habib.usa2014@gmail.com>
2018-08-08 21:14:41 +0600

    Initial state

---
+++ habib/DetectFace.py
@@ -0,0 +1,8 @@
+total 1848
+drwxr-xr-x  73 Habib  staff    2482 Aug 15 17:27 DataStore
+-rw-r--r--@  1 Habib  staff       0 Sep 19 11:16 DetectFace.py
+-rw-r--r--@  1 Habib  staff     745 Mar 13  2017 TrainData.py
+-rw-r--r--@  1 Habib  staff     662 Mar 13  2017 dataset.py
+-rwxr-xr-x@  1 Habib  staff  930127 Oct 16  2016 haarcascade_frontalface_default.xml
+-rw-r--r--@  1 Habib  staff     755 Mar 19  2017 python.py
+drwxr-xr-x   3 Habib  staff     102 Mar 13  2017 recognizer
